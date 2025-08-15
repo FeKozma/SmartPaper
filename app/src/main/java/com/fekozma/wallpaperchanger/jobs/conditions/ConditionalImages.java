2abe7cbaf01dc5959f5e1dc088c05be3d54f6d71
FeKozma <felix.kozma@gmail.com>
2025-08-15 20:09:19 +0200

    fix: if the current condition is not represented in the gallery, dont pick image with any tag in its category

--- app/src/main/java/com/fekozma/wallpaperchanger/jobs/conditions/ConditionalImages.java
+++ app/src/main/java/com/fekozma/wallpaperchanger/jobs/conditions/ConditionalImages.java
@@ -2,7 +2,9 @@ package com.fekozma.wallpaperchanger.jobs.conditions;
 
 import com.fekozma.wallpaperchanger.database.DBImage;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public abstract class ConditionalImages {
 
@@ -10,6 +12,18 @@ public abstract class ConditionalImages {
 
 	public abstract static class OnImagesLoaded {
 		public abstract void onImagesLoaded(List<DBImage> images);
+	}
+
+	public static boolean noCommonElements(List<String> list1, List<String> list2) {
+		// Use a Set for fast lookup
+		Set<String> set = new HashSet<>(list1);
+
+		for (String s : list2) {
+			if (set.contains(s)) {
+				return false; // Found a match
+			}
+		}
+		return true; // No matches
 	}
 
 }
